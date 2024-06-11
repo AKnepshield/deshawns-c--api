f9b5e20b8f48efeec697703642e6ce9f97f18b06
Charles Knepshield <aknepshield@gmail.com>
2024-06-11 12:28:33 -0500

    first commit

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -21,20 +21,31 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+var stuff = Array.Empty<int>();
+
+
 app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.MapGet("/hello", (string? name) =>
+    {
+        name ??= "World";
+        return $"Hello {name}!";
+    })
+    .WithName("GetHello")
+    .WithOpenApi();
 
 app.Run();
 
